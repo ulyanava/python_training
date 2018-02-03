7b58f81319339d2e70691cdec38f7fcd934d2eec
ulyanava <taniauljanova@gmail.com>
2018-02-03 23:56:30 +0300

    Added class Contact

---
+++ contact.py
@@ -0,0 +1,30 @@
+
+class Contact:
+    def __init__(self, first_name, middle_name, last_name, nickname, title, company, address, phone_home,
+                 phone_mobile, phone_work, fax, email_1, email_2, email_3, homepage,  birthday_day, birthday_month,
+                 birthday_year, anniversary_day, anniversary_month, anniversary_year, address_2,
+                 phone_home_2, notes):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.phone_home = phone_home
+        self.phone_mobile = phone_mobile
+        self.phone_work = phone_work
+        self.fax = fax
+        self.email_1 = email_1
+        self.email_2 = email_2
+        self.email_3 = email_3
+        self.homepage = homepage
+        self.birthday_day = birthday_day
+        self.birthday_month = birthday_month
+        self.birthday_year = birthday_year
+        self.anniversary_day = anniversary_day
+        self.anniversary_month = anniversary_month
+        self.anniversary_year = anniversary_year
+        self.address_2 = address_2
+        self.phone_home_2 = phone_home_2
+        self.notes = notes
