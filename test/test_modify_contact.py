cb5030b8b7dd1386d579c8a477d2aa534bdfb5cb
ulyanava <taniauljanova@gmail.com>
2018-02-10 14:58:12 +0300

    Add test modify contact

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,12 +3,24 @@ from python_training.model.contact import Contact
 
 def test_modify_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.modify_first_contact(Contact(first_name="Tania", middle_name="TU", last_name="Ulyanava",
-                                             nickname="tatianka", title="QA", company="Solarwinds", address="Minsk RB",
-                                             phone_home="80172608247",
-                                             phone_mobile="80297758679", phone_work="123", fax="123",
-                                             email_1="1@gmail.com", email_2="2@gmail.com", email_3="3@gmail.com",
-                                             homepage="https://test",
-                                             address_2="", phone_home_2="", notes="",
+    app.contact.modify_first_contact(Contact(first_name="Tania_modified", middle_name="TU_modified",
+                                             last_name="Ulyanava_modified",
+                                             nickname="tatianka_modified",
+                                             title="QA_modified", company="Solarwinds_modified",
+                                             address="Minsk RB_modified",
+                                             phone_home="80172608247_modified",
+                                             phone_mobile="80297758679_modified",
+                                             phone_work="123_modified", fax="123_modified",
+                                             email_1="1_modified@gmail.com",
+                                             email_2="2_modified@gmail.com",
+                                             email_3="3_modified@gmail.com",
+                                             homepage="https://test_modified",
+                                             birthday_day="",
+                                             birthday_month="",
+                                             birthday_year="",
+                                             anniversary_day="",
+                                             anniversary_month="",
+                                             anniversary_year="",
+                                             address_2="_modified", phone_home_2="_modified", notes="_modified",
                                              group="//div[@id='content']/form/select[5]//option[1]"))
     app.session.logout()
